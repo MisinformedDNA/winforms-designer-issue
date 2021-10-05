f4a84b98fa151f05250a3df0155105edf598fbfb
Dan Friedman <dan@thefriedmans.org>
2021-10-05 13:40:05 -0500

    After Designer edit

--- NBi_CommonScreens/UserControls/XtraGridSearch.Designer.cs
+++ NBi_CommonScreens/UserControls/XtraGridSearch.Designer.cs
@@ -30,49 +30,37 @@
         {
             this.components = new System.ComponentModel.Container();
             this.txtSearch = new System.Windows.Forms.TextBox();
-            this.phoenixApplicationSearchFieldTableAdapter = new NBi_DataModule.NBi_DataSetTableAdapters.PhoenixApplicationSearchFieldTableAdapter();
-            this.nBi_DataSet = new NBi_DataModule.NBi_DataSet();
             this.phoenixApplicationSearchFieldBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.txtSearch)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nBi_DataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.phoenixApplicationSearchFieldBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // txtSearch
             // 
             this.txtSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtSearch.Location = new System.Drawing.Point(125, 0);
+            this.txtSearch.Location = new System.Drawing.Point(146, 0);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(179, 20);
+            this.txtSearch.Size = new System.Drawing.Size(208, 23);
             this.txtSearch.TabIndex = 2;
+            this.txtSearch.Text = "k";
             this.txtSearch.Enter += new System.EventHandler(this.txtSearch_Enter);
             this.txtSearch.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtSearch_KeyUp);
-            // 
-            // phoenixApplicationSearchFieldTableAdapter
-            // 
-            this.phoenixApplicationSearchFieldTableAdapter.ClearBeforeFill = true;
-            // 
-            // nBi_DataSet
-            // 
-            this.nBi_DataSet.DataSetName = "NBi_DataSet";
-            this.nBi_DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // phoenixApplicationSearchFieldBindingSource
             // 
             this.phoenixApplicationSearchFieldBindingSource.DataMember = "PhoenixApplicationSearchField";
-            this.phoenixApplicationSearchFieldBindingSource.DataSource = this.nBi_DataSet;
             // 
             // XtraGridSearch
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.txtSearch);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "XtraGridSearch";
-            this.Size = new System.Drawing.Size(305, 20);
-            ((System.ComponentModel.ISupportInitialize)(this.txtSearch)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nBi_DataSet)).EndInit();
+            this.Size = new System.Drawing.Size(356, 23);
             ((System.ComponentModel.ISupportInitialize)(this.phoenixApplicationSearchFieldBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
